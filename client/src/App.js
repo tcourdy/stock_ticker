fdd0c5877cedd811909eab57642876c09e8fdbe8
tcourdy <tarik.courdy@gmail.com>
2021-07-01 22:01:53 -0600

    updated api to use alpaca instead of alpha vantage

--- client/src/App.js
+++ client/src/App.js
@@ -1,83 +1,78 @@
 import React from 'react';
-import './App.css';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
 
 var host = "http://localhost:6969"
 
 class App extends React.Component {
   constructor(props) {
     super(props)
-    this.state = {"symbols":[]}
-  }
-
-  // alphavantage's free api only allows 5 calls per minute so group symbols into groups of 5
-  createTickers(symbols) {
-    if(symbols) {
-      var numGroups = Math.ceil(symbols.length / 5)
-      return symbols.map((ticker, index) => {
-        return(
-          <Ticker symbol={ticker} groupId={index % numGroups} numGroups={numGroups} />
-        )
-      })
-    } else {
-      return []
-    }
-
-  }
-
-  async componentDidMount() {
-    var resp = await fetch(`${host}/getSymbols`)
-    var data = await resp.json()
-    this.setState({
-      "symbols": data.symbols
-    })
-  }
-
-  render() {
-    return (
-      <div className="App">
-        {this.createTickers(this.state.symbols)}
-      </div>
-    );
-  }
-}
-
-class Ticker extends React.Component{
-  constructor(props) {
-    super(props);
-    this.state = {"price": 0.0, "dollarChange": 0.0, "percentChange": 0.0, "numApiAttempts": 0}
+    this.state = {"watchList":{}}
+    this.fetchWatchList = this.fetchWatchList.bind(this)
+    this.createTickerRows = this.createTickerRows.bind(this)
+    this.sortedWatchListByPercent = this.sortedWatchListByPercent.bind(this)
   }
 
   componentDidMount() {
-    this.timerInterval = setInterval(this.fetchCurrentPrice, 60000)
+    this.fetchWatchList()
+    this.timerInterval = setInterval(this.fetchWatchList, 60000)
   }
 
   componentWillUnmount() {
     clearInterval(this.timerInterval)
   }
 
-  async fetchCurrentPrice() {
-    if(this.state.numApiAttempts % this.props.numGroups == this.props.groupId) {
-      var resp = await fetch(`${host}/getPrice/${this.props.symbol}`)
-      var data = await resp.json()
-      var currentPrice = data.currentPrice
-      var dollarChange = currentPrice - this.state.price
-      var percentChange = this.state.price != 0 ? (dollarChange/this.state.price) * 100 : 0
-      this.setState({
-        "price": currentPrice,
-        "dollarChange": dollarChange,
-        "percentChange": percentChange
-      })
-    }
-
+  async fetchWatchList() {
+    var resp = await fetch(`${host}/getWatchList`)
+    var watchList = await resp.json()
+    console.log(watchList)
     this.setState({
-      "numApiAttempts": this.state.numApiAttempts + 1
+      "watchList": watchList
+    })
+  }
+
+  createTickerRows() {
+    return this.sortedWatchListByPercent().map(ticker => {
+      return (
+        <TableRow key={ticker[0]}>
+          <TableCell>{ticker[0]}</TableCell>
+          <TableCell>{ticker[1].currentPrice}</TableCell>
+          <TableCell>{ticker[1].dollarChange}</TableCell>
+          <TableCell>{ticker[1].percentChange}%</TableCell>
+        </TableRow>
+      )
+    })
+  }
+
+  sortedWatchListByPercent() {
+    return Object.entries(this.state.watchList).sort((a, b) => {
+      return b[1].percentChange  - a[1].percentChange
     })
   }
 
   render() {
     return (
-      <h4>{this.props.symbol}: {this.state.price}, {this.state.dollarChange}, {this.state.percentChange}</h4>
-    )
+      <TableContainer component={Paper}>
+        <Table>
+          <TableHead>
+            <TableRow>
+              <TableCell>Symbol</TableCell>
+              <TableCell>Price</TableCell>
+              <TableCell>Dollar Change</TableCell>
+              <TableCell>Percent Change</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.createTickerRows()}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    );
   }
 }
 
--- server/index.js
+++ server/index.js
@@ -5,27 +5,42 @@ const fs = require('fs')
 const app = express()
 const port = 6969
 
-const ALPHA_API_KEY = "alphaApiKey"
+const ALPACA_API_KEY = "alpacaApiKey"
+const ALPACA_SECRET_KEY = "alpacaSecretKey"
 const SYMBOLS_KEY = "symbols"
 
 var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'))
 
-if(config[ALPHA_API_KEY] === undefined || config[SYMBOLS_KEY] === undefined) {
-  console.err("You must provide an alphaApiKey property with a string value and a symbols property with a value of an array of strings")
+if(config[ALPACA_API_KEY] === undefined || config[SYMBOLS_KEY] === undefined || config[ALPACA_SECRET_KEY] === undefined) {
+  console.err("You must provide an alpacaApiKey property with a string value, an alpacaSecretKey property with a string value, and a symbols property with a value of an array of strings")
   process.exit(1)
 }
 
-app.get('/getPrice/:symbol', async (req, res) => {
-  let resp = await axios.get(`https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${req.params.symbol}&interval=1min&apikey=${config[ALPHA_API_KEY]}`);
+//alpaca
+app.get('/getWatchList', async (req, res) => {
+  let httpConfig = {
+    "headers": {
+      'APCA-API-KEY-ID': config[ALPACA_API_KEY],
+      'APCA-API-SECRET-KEY':config[ALPACA_SECRET_KEY]
+    }
+  }
+  let resp = await axios.get(`https://data.alpaca.markets/v2/stocks/snapshots?symbols=${config[SYMBOLS_KEY].join()}`, httpConfig);
 
-  let mostRecentKey = resp.data["Meta Data"]["3. Last Refreshed"]
-  let currentPrice = resp.data["Time Series (1min)"][mostRecentKey]["4. close"]
+  var watchList = {}
+  Object.keys(resp.data).forEach(ticker => {
+    var currentPrice = resp.data[ticker]["minuteBar"]["c"]
+    var daysOpenPrice = resp.data[ticker]["prevDailyBar"]["c"]
+    watchList[ticker] = {
+      "currentPrice": currentPrice,
+      "dollarChange": (currentPrice - daysOpenPrice).toFixed(2),
+      "percentChange": (((currentPrice - daysOpenPrice) / daysOpenPrice) * 100).toFixed(2)
+    }
+  })
 
-  res.status(200).json({"currentPrice": parseFloat(currentPrice)})
+  res.status(200).json(watchList)
 })
 
 app.get('/getSymbols', (req, res) => {
-  console.log(`getSymbols was called: ${Date.now()}`)
   res.status(200).json({"symbols": config[SYMBOLS_KEY]})
 })
 
